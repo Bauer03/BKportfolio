cea0b8df450cc7c6187f552bf0dc1f8557c942c6
PaulxT0 <Paulalexisbauer04@gmail.com>
2024-04-20 14:19:08 -0400

    Change event generation
    • Change event generation to be dynamic so that BK can add images/events to the 'events' array and have them automatically be generated on page load

--- out/events.js
+++ out/events.js
@@ -1,28 +1,64 @@
 "use strict";
 let eventsCont = document.querySelector(".events-cont");
-let luminoCity = document.querySelector(".lumino-city");
-let luminoToggle = document.querySelector(".lumino-toggle");
-let artShow = document.querySelector(".artshow");
-let artShowToggle = document.querySelector(".artshow-toggle");
 eventsCont.style.top = (navbar.getBoundingClientRect().height) + "px";
 eventsCont.style.height = window.innerHeight - navbar.getBoundingClientRect().height + "px";
-luminoToggle.addEventListener("click", () => {
-    if (luminoCity.classList.contains("open")) {
-        luminoCity.classList.remove("open");
-        // luminoToggle.textContent = "close";
-    }
-    else {
-        luminoCity.classList.add("open");
-        // luminoToggle.textContent = "more";
-    }
-});
-artShowToggle.addEventListener("click", () => {
-    if (artShow.classList.contains("open")) {
-        artShow.classList.remove("open");
-        // artShowToggle.textContent = "close";
-    }
-    else {
-        artShow.classList.add("open");
-        // artShowToggle.textContent = "more";
-    }
-});
+let events = [
+    {
+        title: "LuminoCity",
+        desc: "This is the description of the event. It will be a paragraph or two long, and will describe the event in detail.",
+        images: [
+            'images/art/digital/image.jpeg',
+            'images/art/digital/image.jpeg',
+        ]
+    },
+    {
+        title: "Art Show",
+        desc: "This is the description of the event. It will be a paragraph or two long, and will describe the event in detail.",
+        images: [
+            'images/art/digital/image.jpeg',
+            'images/art/digital/image.jpeg',
+        ]
+    },
+];
+for (let evt of events) {
+    let temp = getEventHTML(evt);
+    eventsCont.appendChild(temp);
+    temp.addEventListener("click", () => {
+        toggleSection(temp.children[0]);
+    });
+}
+function toggleSection(toggleThis) {
+    toggleThis.classList.toggle("open");
+}
+function getEventHTML(evt) {
+    let temp = document.createElement("div");
+    temp.className = "event";
+    let imgGallery = [];
+    evt.images.forEach((img) => {
+        let tempImg = document.createElement("img");
+        tempImg.className = "event-img";
+        tempImg.src = `${img}`;
+        imgGallery.push(tempImg);
+    });
+    temp.innerHTML = `
+        <div class="event lumino-city">
+            <div class="event-top">
+                <div class="event-title">
+                    ${evt.title}
+                </div>
+                <div class="section-toggle">
+                    <span class="material-symbols-outlined lumino-toggle">expand_more</span>
+                </div>
+            </div>
+            <div class="event-bottom" t="${evt.title}">
+                <div class="event-image-gallery">
+                    ${imgGallery}
+                </div>
+                <div class="event-desc">
+                    ${evt.desc}
+                </div>
+            </div>
+        </div>
+    `;
+    return temp;
+}
--- src/events.ts
+++ src/events.ts
@@ -1,28 +1,76 @@
 let eventsCont = document.querySelector(".events-cont") as HTMLElement;
-let luminoCity = document.querySelector(".lumino-city") as HTMLElement;
-let luminoToggle = document.querySelector(".lumino-toggle") as HTMLElement;
-let artShow = document.querySelector(".artshow") as HTMLElement;
-let artShowToggle = document.querySelector(".artshow-toggle") as HTMLElement;
-
 eventsCont.style.top = (navbar.getBoundingClientRect().height) + "px";
 eventsCont.style.height = window.innerHeight - navbar.getBoundingClientRect().height + "px";
 
-luminoToggle.addEventListener("click", () => {
-    if(luminoCity.classList.contains("open")) {
-        luminoCity.classList.remove("open");
-        // luminoToggle.textContent = "close";
-    } else {
-        luminoCity.classList.add("open");
-        // luminoToggle.textContent = "more";
-    }
-});
+interface ArtEvent {
+    title:String;
+    desc:String;
+    images:String[];
+}
 
-artShowToggle.addEventListener("click", () => {
-    if(artShow.classList.contains("open")) {
-        artShow.classList.remove("open");
-        // artShowToggle.textContent = "close";
-    } else {
-        artShow.classList.add("open");
-        // artShowToggle.textContent = "more";
-    }
-});
+let events: ArtEvent[] = [
+    {
+        title: "LuminoCity",
+        desc: "This is the description of the event. It will be a paragraph or two long, and will describe the event in detail.",
+        images: [
+            'images/art/digital/image.jpeg', // To add more images to an event, add the image to the 'images' folder and add the path on a new line.
+            'images/art/digital/image.jpeg',
+        ]
+    },
+    {
+        title: "Art Show",
+        desc: "This is the description of the event. It will be a paragraph or two long, and will describe the event in detail.",
+        images: [
+            'images/art/digital/image.jpeg',
+            'images/art/digital/image.jpeg',
+        ]
+    },
+];
+
+for(let evt of events) {
+    let temp = getEventHTML(evt);
+    eventsCont.appendChild(temp);
+    temp.addEventListener("click", () => {
+        toggleSection(temp.children[0] as HTMLElement);
+    })
+}
+
+function toggleSection(toggleThis:HTMLElement) {
+    toggleThis.classList.toggle("open");
+}
+
+function getEventHTML(evt:ArtEvent) {
+    let temp = document.createElement("div");
+    temp.className = "event";
+    let imgGallery: HTMLSpanElement[] = [];
+
+    evt.images.forEach((img) => {
+        let tempImg:HTMLImageElement = document.createElement("img");
+        tempImg.className = "event-img";
+        tempImg.src = `${img}`;
+        imgGallery.push(tempImg);
+    })
+
+    temp.innerHTML = `
+        <div class="event lumino-city">
+            <div class="event-top">
+                <div class="event-title">
+                    ${evt.title}
+                </div>
+                <div class="section-toggle">
+                    <span class="material-symbols-outlined lumino-toggle">expand_more</span>
+                </div>
+            </div>
+            <div class="event-bottom" t="${evt.title}">
+                <div class="event-image-gallery">
+                    ${imgGallery}
+                </div>
+                <div class="event-desc">
+                    ${evt.desc}
+                </div>
+            </div>
+        </div>
+    `;
+
+    return temp;
+}
