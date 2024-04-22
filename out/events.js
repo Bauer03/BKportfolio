cafbb7745f5f749cf526bb2d301ff0c3d8ccd7c2
PaulxT0 <Paulalexisbauer04@gmail.com>
2024-04-22 08:35:47 -0400

    Finish basic setup for event page
    • Add functionality to cycling through images
    • Misc fixes to some mistakes I made

--- out/events.js
+++ out/events.js
@@ -8,7 +8,7 @@ for (let evt of events) {
     temp.addEventListener("click", (event) => {
         let obj = event.target;
         if (!(obj.classList.contains("nT"))) {
-            toggleSection(temp.children[0]);
+            toggleSection(temp);
         }
     });
 }
@@ -23,26 +23,38 @@ function getEventHTML(evt) {
     temp.className = "event";
     let imageIndex = 0;
     temp.innerHTML = `
-        <div class="event">
-            <div class="event-top">
-                <div class="event-title">
-                    ${evt.title}
-                </div>
-                <div class="section-toggle">
-                    <span class="material-symbols-outlined toggle">expand_more</span>
-                </div>
+        <div class="event-top">
+            <div class="event-title">
+                ${evt.title}
             </div>
-            <div class="event-bottom">
-                <div class="event-image-gallery nT">
-                    <span class="material-symbols-outlined nT">arrow_left</span>
-                    <img src="${evt.images[imageIndex++]}" class="nT">
-                    <span class="material-symbols-outlined nT">arrow_right</span>
-                </div>
-                <div class="event-desc">
-                    ${evt.desc}
-                </div>
+            <div class="section-toggle">
+                <span class="material-symbols-outlined toggle">expand_more</span>
+            </div>
+        </div>
+        <div class="event-bottom">
+            <div class="event-image-gallery nT">
+                <span class="material-symbols-outlined nT go-left">arrow_left</span>
+                <img src="${evt.images[imageIndex]}" class="nT event-img">
+                <span class="material-symbols-outlined nT go-right">arrow_right</span>
+            </div>
+            <div class="event-desc">
+                ${evt.desc}
             </div>
         </div>
     `;
+    let img = temp.querySelector(".event-img");
+    let right = temp.querySelector(".go-right");
+    right.addEventListener("click", (event) => {
+        imageIndex = imageIndex < (evt.images.length - 1) ? imageIndex + 1 : 0;
+        swapImg(img, evt.images[imageIndex]);
+    });
+    let left = temp.querySelector(".go-left");
+    left.addEventListener("click", (event) => {
+        imageIndex = imageIndex > 0 ? imageIndex - 1 : (evt.images.length - 1);
+        swapImg(img, evt.images[imageIndex]);
+    });
     return temp;
+}
+function swapImg(img, imgURL) {
+    img.src = imgURL;
 }
--- src/events.ts
+++ src/events.ts
@@ -8,7 +8,7 @@ for(let evt of events) {
     temp.addEventListener("click", (event) => {
         let obj = event.target as HTMLElement;
         if(!(obj.classList.contains("nT"))){
-            toggleSection(temp.children[0] as HTMLElement);
+            toggleSection(temp as HTMLElement);
         }
     });
 }
@@ -26,27 +26,43 @@ function getEventHTML(evt:ArtEvent) {
     let imageIndex:number = 0;
 
     temp.innerHTML = `
-        <div class="event">
-            <div class="event-top">
-                <div class="event-title">
-                    ${evt.title}
-                </div>
-                <div class="section-toggle">
-                    <span class="material-symbols-outlined toggle">expand_more</span>
-                </div>
+        <div class="event-top">
+            <div class="event-title">
+                ${evt.title}
             </div>
-            <div class="event-bottom">
-                <div class="event-image-gallery nT">
-                    <span class="material-symbols-outlined nT">arrow_left</span>
-                    <img src="${evt.images[imageIndex++]}" class="nT">
-                    <span class="material-symbols-outlined nT">arrow_right</span>
-                </div>
-                <div class="event-desc">
-                    ${evt.desc}
-                </div>
+            <div class="section-toggle">
+                <span class="material-symbols-outlined toggle">expand_more</span>
+            </div>
+        </div>
+        <div class="event-bottom">
+            <div class="event-image-gallery nT">
+                <span class="material-symbols-outlined nT go-left">arrow_left</span>
+                <img src="${evt.images[imageIndex]}" class="nT event-img">
+                <span class="material-symbols-outlined nT go-right">arrow_right</span>
+            </div>
+            <div class="event-desc">
+                ${evt.desc}
             </div>
         </div>
     `;
 
+    let img = temp.querySelector(".event-img") as HTMLImageElement;
+
+    let right = temp.querySelector(".go-right") as HTMLElement;
+    right.addEventListener("click", (event) => {
+        imageIndex = imageIndex < (evt.images.length - 1) ? imageIndex + 1 : 0;
+        swapImg(img,evt.images[imageIndex]);
+    })
+
+    let left = temp.querySelector(".go-left") as HTMLElement;
+    left.addEventListener("click", (event) => {
+        imageIndex = imageIndex > 0 ? imageIndex - 1 : (evt.images.length - 1);
+        swapImg(img,evt.images[imageIndex]);
+    })
+
     return temp;
+}
+
+function swapImg(img:HTMLImageElement,imgURL:String) {
+    img.src = imgURL as string;
 }
